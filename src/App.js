f744b319255956c05f89e527ad6fbff9a3d0422b
brendan sudol <brendansudol@gmail.com>
2020-02-23 12:14:09 -0500

    add flex grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { Flex, Box } from "rebass"
 import { makeFace } from "./utils"
 
 const size = 256
@@ -6,16 +7,18 @@ const dataUrl = `${process.env.PUBLIC_URL}/data/doodles.json`
 
 const styles = {
   svg: {
-    border: "1px solid #ccc"
+    border: "1px solid #ccc",
+    width: "100%",
+    height: "auto"
   },
   path: {
     fill: "none",
     stroke: "tomato",
-    strokeWidth: 3
+    strokeWidth: 4
   }
 }
 
-const getFaces = (data, n = 4) => [...Array(n)].map(_ => makeFace(data))
+const getFaces = (data, n = 9) => [...Array(n)].map(_ => makeFace(data))
 
 export const App = React.memo(() => {
   const [data, setData] = useState(null)
@@ -37,22 +40,24 @@ export const App = React.memo(() => {
   if (!faces) return null
 
   return (
-    <div style={{ padding: "1rem", maxWidth: 600 }}>
-      {faces.map((face, i) => (
-        <svg
-          key={i}
-          width={size}
-          height={size}
-          viewBox={`0 0 ${size} ${size}`}
-          style={styles.svg}
-        >
-          <path d={face.circle} style={styles.path} />
-          <path d={face.nose} style={styles.path} />
-          <path d={face.mouth} style={styles.path} />
-          <path d={face.eyeLeft} style={styles.path} />
-          <path d={face.eyeRight} style={styles.path} />
-        </svg>
-      ))}
+    <div style={{ padding: "1rem", margin: "0 auto", maxWidth: 600 }}>
+      <Flex flexWrap="wrap" mx={-2}>
+        {faces.map((face, i) => (
+          <Box key={i} px={2} py={2} width={1 / 3}>
+            <svg
+              viewBox={`0 0 ${size} ${size}`}
+              style={styles.svg}
+              preserveAspectRatio="none"
+            >
+              <path d={face.circle} style={styles.path} />
+              <path d={face.nose} style={styles.path} />
+              <path d={face.mouth} style={styles.path} />
+              <path d={face.eyeLeft} style={styles.path} />
+              <path d={face.eyeRight} style={styles.path} />
+            </svg>
+          </Box>
+        ))}
+      </Flex>
 
       <br />
 
